64a358a1e8c6cdcda585960545b18e9f65cdbe7e
David <dave00dark@gmail.com>
2019-10-11 22:04:11 -0400

    App para manejo de Usuarios

---
+++ usuarios/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Authentication
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+# CRUD User
+def create_user(request):
+    pass
+
+def modifi_user(request):
+    pass
+
+def delete_user(request):
+    pass
+
+def change_password_user(request):
+    pass
