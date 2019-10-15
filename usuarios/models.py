2af84d03b454876a4da8e0112c7de668c52c058f
David <dave00dark@gmail.com>
2019-10-15 17:53:22 -0400

    Modificacion de usuario, email como username

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField('email address', unique=True)
+    username = models.CharField(max_length=100, verbose_name='Nombre de Usuario')
+    REQUIRED_FIELDS = ['username'] 
