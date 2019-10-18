3523d4f9f0275f1dfc2cdada639b0d44097eae03
David <dave00dark@gmail.com>
2019-10-17 22:41:15 -0400

    CRUD User Ready

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import login, logout, create_user, modify_user, delete_user, change_password_user
+from .views import login_user, logout_user, create_user, modify_user, delete_user, change_password_user
 
 app_name = "usuarios"
 
 urlpatterns = [
     path('create-user', create_user, name="create_user"),
-    path('modify-user/<int:pk>', modify_user, name="modify_user")
+    path('modify-user/<int:pk>', modify_user, name="modify_user"),
+    path('login', login_user, name="login"),
+    path('logout', logout_user, name="logout"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render, redirect
-from .forms import UserCreationForm, UserChangeForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserCreationForm, UserChangeForm, UserLoginForm
+from .models import User
 
 # Authentication
-def login(request):
-    pass
+def login_user(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
 
-def logout(request):
-    pass
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+
+            return redirect("oikos:home")
+        else:
+            messages.error(request,"Email o Password Incorrecto")
+        return redirect("usuarios:login")
+    else:
+        form = UserLoginForm()
+        return render(request, "usuarios/create_user.html", {"form" : form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect("oikos:home")
 
 # CRUD User
 def create_user(request):
@@ -18,20 +39,35 @@ def create_user(request):
             messages.success(request, "Usuario Agregado")
         else:
             messages.error(request,"Datos Inalidos")
-        return redirect("usuarios:create_user")
+        return redirect("oikos:home")
     else:
         form = UserCreationForm()
-
         return render(request, "usuarios/create_user.html", {"form" : form})
 
+@login_required
 def modify_user(request, pk, *args, **kwargs):
     if request.method == "POST":
-
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Usuario Modificado")
+        else:
+            messages.error(request,"Datos Inalidos")
+        return redirect("usuarios:modify_user", pk=pk)
     else:
         user = User.objects.get(pk = pk)
-        form = UserChangeForm()
-def delete_user(request):
-    pass
+        form = UserChangeForm(instance=user)
+        return render(request, "usuarios/create_user.html", {"form" : form})
 
+@login_required
+def delete_user(request, pk, *args, **kwargs):
+    if request.method == "POST":
+        user = User.objects.get(pk = pk)
+        user.delete()
+        messages.error(request,"Usuario Eliminado")
+    
+    return redirect("oikos:home")
+
+@login_required
 def change_password_user(request):
     pass
