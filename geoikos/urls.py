93fbc3aae3977771633bee66cc918b8ec725e493
MarcosJLR <socram.693@gmail.com>
2019-10-24 14:56:09 -0400

    Added urls for inventory

--- geoikos/urls.py
+++ geoikos/urls.py
@@ -20,7 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Users/', include("Users.urls")),
-
-    # path('',views.home),
+    path('Inventory/', include("Inventory.urls")),
     path('', include("oikos.urls")),
 ]
