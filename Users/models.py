e3d588286bd1d6d54aee938f85dd44568113f79b
MarcosJLR <socram.693@gmail.com>
2019-11-14 16:16:15 -0400

    Added a __str__() for user

--- Users/models.py
+++ Users/models.py
@@ -16,3 +16,6 @@ class User(AbstractUser):
     balance = models.IntegerField(null=True)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES,null=True, default='IN')
     REQUIRED_FIELDS = ['username'] 
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' (' + self.username + ')'
