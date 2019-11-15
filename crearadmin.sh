9d47dc0706e517bbee9b4006e413d8ac62bd5b23
David <dave00dark@gmail.com>
2019-11-15 11:24:55 -0400

    script de crear admin

---
+++ crearadmin.sh
@@ -0,0 +1,1 @@
+echo "from Users.models import User;from django.contrib.auth.models import Group;u = User.objects.create_superuser(username='admin',email='admin@admin.com',password='da123456');u.groups.add(Group.objects.get(name='admin'))" | python manage.py shell
