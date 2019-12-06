d6885d193940418d599d9cf21ad6ea8932a70c13
NJaua <nicojaua@gmail.com>
2019-12-06 02:58:32 -0400

    Carga imagenes de perfil y las eneseña, pero por erro en carga de usuario no se puede corrborar como afecta pasar distintas fotos

--- Users/views.py
+++ Users/views.py
@@ -40,7 +40,7 @@ def logout_user(request):
 @is_gestor_usuario
 def create_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(request.POST,request.FILES)
         if form.is_valid():
             user = form.save()
             try:
@@ -71,7 +71,7 @@ def create_user(request):
 def modify_user(request, pk, *args, **kwargs):
     if request.method == "POST":
         instance = User.objects.get(pk=pk)
-        form = UserChangeForm(request.POST, instance=instance)
+        form = UserChangeForm(request.POST, request.FILES, instance=instance)
         if form.is_valid():
             user = form.save()
             if request.user.groups.filter(Q(name='admin') | Q(name='gestor_usuarios')).exists():
@@ -87,7 +87,7 @@ def modify_user(request, pk, *args, **kwargs):
                     user.save()
             messages.success(request, "Usuario Modificado")
         else:
-            messages.error(request,"Datos Inalidos")
+            messages.error(request,"Datos Inválidos")
         return redirect("Inventory:home_inventory")
     
     if((request.user.groups.first().name != 'admin' or
